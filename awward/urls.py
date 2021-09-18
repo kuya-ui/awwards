3e4bbf5c37073106fcb3b449f788a335617cb639
kuya-ui <kuyamaxmillan@gmail.com>
2021-09-18 21:38:59 +0300

    make changes

--- awward/urls.py
+++ awward/urls.py
@@ -1,6 +1,23 @@
 from django.conf.urls import url
 from . import views
+from django.urls import path
+from django.conf import settings
+from django.conf.urls import static
+from django.conf.urls.static import static
+
 
 urlpatterns=[
      url(r'^$',views.index,name='home'),
+     path('profile/',views.profile,name = 'profile'),
+     url(r'^newproject/$',views.new_project,name='newproject'),
+     url(r'^search/',views.search_results,name = 'search_results'),
+     path('comment/<int:id>/',views.comment,name='comment'),
+     # path('rate/<int:id>/',views.rate,name='rates'),
+     # url(r'^api/profile/$',views.ProfileList.as_view()),
+     # url(r'^api/projects/$',views.ProfileList.as_view()),
+     url(r'^logout/$',views.logout_request,name='logout')
 ]
+
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
