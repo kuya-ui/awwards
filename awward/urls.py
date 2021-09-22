5580d9f8867a77b79fb14dce9b2aa7f0ca79a6d3
kuya-ui <kuyamaxmillan@gmail.com>
2021-09-22 19:20:34 +0300

    make change

--- awward/urls.py
+++ awward/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url,include
 from . import views
-# from django.urls import path
+
 from django.conf import settings
 from django.conf.urls import static
 from django.conf.urls.static import static
@@ -9,10 +9,10 @@ from django.conf.urls.static import static
 urlpatterns=[
      url(r'^$',views.index,name='home'),
      url(r'^newproject/$',views.new_project,name='newproject'),
-     # path('profile/',views.profile,name = 'profile'),
+     url(r'profile/',views.profile,name = 'profile'),
      url(r'^search/',views.search_results,name = 'search_results'),
-     # path('comment/<int:id>/',views.comment,name='comment'),
-     # path('rate/<int:id>/',views.rate,name='rates'),
+     url(r'comment/<int:id>/',views.comment,name='comment'),
+     url(r'rate/<int:id>/',views.rate,name='rates'),
      url(r'^singleproject/(\d+)',views.single_project,name='singleproject'),
      url(r'^editprofile/$',views.edit_profile,name='editprofile'),
      url(r'^logout/$',views.logout_request,name='logout')
